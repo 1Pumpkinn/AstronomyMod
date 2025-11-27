7096ac7768aaa1699ee685b1d57a49eacc503bf1
Pumpkin <wishsmp1002@gmail.com>
2025-11-28 02:24:02 +1100

    Kinda fixed creative slot

--- src/main/java/hs/astronomymod/client/AstronomymodDataGenerator.java
+++ src/main/java/hs/astronomymod/client/AstronomymodDataGenerator.java
@@ -1,5 +1,8 @@
 package hs.astronomymod.client;
 
+import hs.astronomymod.datagen.ModItemTagProvider;
+import hs.astronomymod.datagen.ModModelProvider;
+import hs.astronomymod.datagen.ModRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -8,5 +11,10 @@ public class AstronomymodDataGenerator implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+        // Register providers
+        pack.addProvider(ModModelProvider::new);
+        pack.addProvider(ModRecipeProvider::new);
+        pack.addProvider(ModItemTagProvider::new);
     }
 }
---
+++ src/main/java/hs/astronomymod/datagen/ModModelProvider.java
@@ -0,0 +1,31 @@
+package hs.astronomymod.datagen;
+
+import hs.astronomymod.item.ModItems;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.client.data.BlockStateModelGenerator;
+import net.minecraft.client.data.ItemModelGenerator;
+import net.minecraft.client.data.Models;
+
+public class ModModelProvider extends FabricModelProvider {
+
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        // Add block models here when you have blocks
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        // Generate basic item models
+        itemModelGenerator.register(ModItems.PLANET_ITEM, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SUPERNOVA_ITEM, Models.GENERATED);
+        itemModelGenerator.register(ModItems.BLACKHOLE_ITEM, Models.GENERATED);
+        itemModelGenerator.register(ModItems.NEUTRON_STAR_ITEM, Models.GENERATED);
+        itemModelGenerator.register(ModItems.PULSAR_ITEM, Models.GENERATED);
+        itemModelGenerator.register(ModItems.ASTRONOMY_SHARD, Models.GENERATED);
+    }
+}
